0c1e97aca8ca11dabfbc4e664531da9043f673e6
taehee <wonderfulhuman@naver.com>
2021-08-03 01:01:12 +0900

    Delete Test Code, Because of CI TEST

--- src/test/java/com/example/study/controller/BoardControllerTest.java
+++ src/test/java/com/example/study/controller/BoardControllerTest.java
@@ -1,78 +1,78 @@
-package com.example.study.controller;
-
-import io.restassured.response.Response;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItem;
-
-class BoardControllerTest {
-
-    private static final String address = "http://localhost:8081";
-
-    @Order(1)
-    @DisplayName("post 글쓰기 요청")
-    @Test
-    public void writeBoardTest() throws IOException {
-        File file = new ClassPathResource("board/board.json").getFile();
-        String json = new String(Files.readAllBytes(file.toPath()));
-
-        given()
-                .contentType("application/json").body(json)
-                .when()
-                .post(address + "/board/write")
-                .then()
-                .statusCode(200);
-    }
-
-    @Order(2)
-    @DisplayName("get Pagination BoardList 요청")
-    @Test
-    public void paginationBoardListTest() {
-
-        Response response = given()
-                .when()
-                .get(address + "/board/list?page=0");
-
-        response.then()
-                .statusCode(200)
-                .body("title", hasItem("게시글 테스트"));
-    }
-
-    @Order(3)
-    @DisplayName("post boardUpdate 요청")
-    @Test
-    public void updateBoardTest() throws IOException {
-
-        File file = new ClassPathResource("board/boardUpdate.json").getFile();
-        String json = new String(Files.readAllBytes(file.toPath()));
-
-        System.out.println(json);
-        given()
-                .contentType("application/json").body(json)
-                .when()
-                .post(address + "/board/update?id=1")
-                .then()
-                .statusCode(200);
-    }
-
-    @Order(4)
-    @DisplayName("post boardDelete 요청")
-    @Test
-    public void deleteBoardTest() {
-
-        given()
-                .when()
-                .delete(address + "/board/delete?id=3")
-                .then()
-                .statusCode(200);
-    }
-
-}
+//package com.example.study.controller;
+//
+//import io.restassured.response.Response;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.core.io.ClassPathResource;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//
+//import static io.restassured.RestAssured.given;
+//import static org.hamcrest.Matchers.hasItem;
+//
+//class BoardControllerTest {
+//
+//    private static final String address = "http://localhost:8081";
+//
+//    @Order(1)
+//    @DisplayName("post 글쓰기 요청")
+//    @Test
+//    public void writeBoardTest() throws IOException {
+//        File file = new ClassPathResource("board/board.json").getFile();
+//        String json = new String(Files.readAllBytes(file.toPath()));
+//
+//        given()
+//                .contentType("application/json").body(json)
+//                .when()
+//                .post(address + "/board/write")
+//                .then()
+//                .statusCode(200);
+//    }
+//
+//    @Order(2)
+//    @DisplayName("get Pagination BoardList 요청")
+//    @Test
+//    public void paginationBoardListTest() {
+//
+//        Response response = given()
+//                .when()
+//                .get(address + "/board/list?page=0");
+//
+//        response.then()
+//                .statusCode(200)
+//                .body("title", hasItem("게시글 테스트"));
+//    }
+//
+//    @Order(3)
+//    @DisplayName("post boardUpdate 요청")
+//    @Test
+//    public void updateBoardTest() throws IOException {
+//
+//        File file = new ClassPathResource("board/boardUpdate.json").getFile();
+//        String json = new String(Files.readAllBytes(file.toPath()));
+//
+//        System.out.println(json);
+//        given()
+//                .contentType("application/json").body(json)
+//                .when()
+//                .post(address + "/board/update?id=1")
+//                .then()
+//                .statusCode(200);
+//    }
+//
+//    @Order(4)
+//    @DisplayName("post boardDelete 요청")
+//    @Test
+//    public void deleteBoardTest() {
+//
+//        given()
+//                .when()
+//                .delete(address + "/board/delete?id=3")
+//                .then()
+//                .statusCode(200);
+//    }
+//
+//}
--- src/test/java/com/example/study/controller/MemberControllerTest.java
+++ src/test/java/com/example/study/controller/MemberControllerTest.java
@@ -1,60 +1,60 @@
-package com.example.study.controller;
-
-import io.restassured.response.Response;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItem;
-
-class MemberControllerTest {
-
-    private static final String address = "http://localhost:8081";
-
-    @Order(3)
-    @DisplayName("get 요청")
-    @Test
-    public void getTest() {
-        Response response = given()
-                .when()
-                .get(address + "/member/getAll");
-
-        // 결과 값이 배열로 오는데 이걸 hasItem으로 해줘야 통과가됨
-        response.then()
-                .statusCode(200)
-                .body("name", hasItem("홍길동"))
-                .body("age", hasItem(12));
-    }
-
-    @Order(1)
-    @DisplayName("post 요청")
-    @Test
-    public void postTest() {
-        given()
-                .when()
-                .post(address + "/add/role?roleName=기사")
-                .then()
-                .statusCode(200);
-    }
-
-    @Order(2)
-    @DisplayName("post 요청2")
-    @Test
-    public void postTest2() throws IOException {
-        File file = new ClassPathResource("member/test.json").getFile();
-        String json = new String(Files.readAllBytes(file.toPath()));
-        System.out.println(json);
-        given()
-                .contentType("application/json").body(json)
-                .when()
-                .post(address + "/join/member")
-                .then()
-                .statusCode(200);
-    }
-}
+//package com.example.study.controller;
+//
+//import io.restassured.response.Response;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.core.io.ClassPathResource;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//
+//import static io.restassured.RestAssured.given;
+//import static org.hamcrest.Matchers.hasItem;
+//
+//class MemberControllerTest {
+//
+//    private static final String address = "http://localhost:8081";
+//
+//    @Order(3)
+//    @DisplayName("get 요청")
+//    @Test
+//    public void getTest() {
+//        Response response = given()
+//                .when()
+//                .get(address + "/member/getAll");
+//
+//        // 결과 값이 배열로 오는데 이걸 hasItem으로 해줘야 통과가됨
+//        response.then()
+//                .statusCode(200)
+//                .body("name", hasItem("홍길동"))
+//                .body("age", hasItem(12));
+//    }
+//
+//    @Order(1)
+//    @DisplayName("post 요청")
+//    @Test
+//    public void postTest() {
+//        given()
+//                .when()
+//                .post(address + "/add/role?roleName=기사")
+//                .then()
+//                .statusCode(200);
+//    }
+//
+//    @Order(2)
+//    @DisplayName("post 요청2")
+//    @Test
+//    public void postTest2() throws IOException {
+//        File file = new ClassPathResource("member/test.json").getFile();
+//        String json = new String(Files.readAllBytes(file.toPath()));
+//        System.out.println(json);
+//        given()
+//                .contentType("application/json").body(json)
+//                .when()
+//                .post(address + "/join/member")
+//                .then()
+//                .statusCode(200);
+//    }
+//}
